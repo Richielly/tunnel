90a4c3a881d0a5ecf5334c3882df7e8713203834
Richielly <richielly.positivo@gmail.com>
2023-09-21 15:40:40 -0300

    V_1.0.16

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
+import os
+
 import flet as ft
 import tunnel as tunnel
 from datetime import datetime
 def main(page: ft.Page):
     page.window_center()
-    page.title = "Redirect Port"
+    page.title = "Redirect Port SCF"
+    page.banner
+
+    path = os.getcwd()
 
     data_atual = datetime.now()
-    data_especifica = datetime(2023, 6, 30)
+    data_especifica = datetime(2023, 12, 30)
 
     def btn_click(e):
         if not txt_port.value:
             txt_port.error_text = "Informe a Porta desejada.!"
             page.update()
         else:
+            # agente = dropdown.value # campo para escolher agente
             port = txt_port.value
+            endpoint = txt_endpoint.value
             url = tunnel.iniciar(port)
             page.clean()
+            # print(tunnel.list_tunnel())
             page.add(ft.Text('Url Para acesso externo'))
             ft.Divider(),
-            new_link = ft.FilledTonalButton(url=f"{url}", text=f"Direcionador porta [{port}] : {url}")
+            new_link = ft.FilledTonalButton(url=f"{url}/{endpoint}", text=f"Direcionador porta [{port}] : {url}/{endpoint}")
             page.add(new_link)
             page.add(ft.ElevatedButton("Fechar todas as conexões", on_click=btn_click_close, icon=ft.icons.CLOSE_OUTLINED,icon_color=ft.colors.ERROR))
             page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-            tunnel.generate_qrcode(url)
-            page.add(ft.Image(src=r"C:\Users\Equiplano\PycharmProjects\tunnel\url-qrcode.png"))
+            tunnel.generate_qrcode(url, endpoint)
+            page.add(ft.Image(src=f"{path}/url-qrcode.png"))
             page.update()
 
     def btn_click_close(e):
@@ -34,13 +42,30 @@ def main(page: ft.Page):
         page.add(ft.Text("Todas as conexões foram finalizadas com sucesso.."))
 
     txt_port = ft.TextField(label="Escolha a porta desejada.")
+    dropdown = ft.Dropdown(
+        label="Agente",
+        hint_text="Escolha o agente.",
+        options=[
+            ft.dropdown.Option("2Vi7N8Xk2LTH0BU5tPDgysmLm10_86EwTxyGZd7kq8HVt5dpv", text="SRH"),
+            ft.dropdown.Option("2Vi83K2j0PzozhcSGfu6l4Yzqma_7RTRY12JfMWFpAy6FQV8M", text="STM"),
+            ft.dropdown.Option("2Vi8MYIwRvifQcHLejg4zUCepWz_3V1nB8qCcw6pVATDhRQQg", text="SCF"),
+        ],
+        autofocus=True,
+    )
+
+    txt_endpoint = ft.TextField(label="endpoint")
     if not data_atual > data_especifica:
-        page.add(ft.Row([txt_port, ft.ElevatedButton("Mapear", on_click=btn_click, icon=ft.icons.LINK)])),
+        page.add(ft.Text("🌎 Aplicativo para criação de Tunelamento de portas local. 🚀"))
+        page.add(ft.Row([txt_port, txt_endpoint, ft.ElevatedButton("Mapear", on_click=btn_click, icon=ft.icons.LINK)])),
+
     # page.add(ft.ElevatedButton("Fechar todas as conexões", on_click=btn_click_close, icon=ft.icons.CLOSE_OUTLINED, icon_color=ft.colors.ERROR)),
 
 if __name__ == "__main__":
-    ft.app(target=main, port=3636)
+    ft.app(target=main)
     # ft.app(port= 3636, target=main, view=ft.WEB_BROWSER)
 
 
-# pyinstaller --name redirect_port --onefile --noconsole main.py
+# pyinstaller --name redirect_port --onefile --icon=img.ico --noconsole main.py
+
+# flet pack --name redirect_port --icon=img.ico main.py
+
--- tunnel.py
+++ tunnel.py
@@ -5,13 +5,15 @@ import requests
 import subprocess
 import ssl
 
-conf.get_default().ngrok_path = os.getcwd()+ r'\ngrok.exe'
+# conf.get_default().ngrok_path = os.getcwd()+ r'\ngrok.exe'
 
-def set_ngrok_auth_token(token = '21YTaWNEb99ey9jMhzHpd_5pdd1qgPoJNcTNryx1SpJ'):
+def set_ngrok_auth_token(token = '2Vi8MYIwRvifQcHLejg4zUCepWz_3V1nB8qCcw6pVATDhRQQg'):
     command = f"ngrok authtoken {token}"
     subprocess.run(command, shell=True)
+    agente = f"ngrok config upgrade"
+    subprocess.run(agente, shell=True)
 
-def start_tunnel(port=2424):
+def start_tunnel(port=4040):
 
     pyngrok_config = conf.get_default()
     if not os.path.exists(pyngrok_config.ngrok_path):
@@ -35,16 +37,22 @@ def list_tunnel():
 
     return tunnels
 
-def iniciar(port=2424):
+def iniciar(port=4040):
     set_ngrok_auth_token()
     public_url = start_tunnel(port)
     return public_url
 
-
-def generate_qrcode(url, filename='url-qrcode.png'):
+def generate_qrcode(url, endpoint, filename='url-qrcode.png'):
     qr = qrcode.QRCode(version=1, box_size=10, border=5)
-    qr.add_data(url)
+    qr.add_data(f"{url}/{endpoint}")
     qr.make(fit=True)
 
     img = qr.make_image(fill='green', back_color='white')
     img.save(filename)
+
+# ngrok config upgrade # Tem que rodar depois de rodar o sistema com o token uma vez.
+
+# Emails= equiplanosrh@gmail.com [2Vi7N8Xk2LTH0BU5tPDgysmLm10_86EwTxyGZd7kq8HVt5dpv]
+# equiplanostm@gmail.com[2Vi83K2j0PzozhcSGfu6l4Yzqma_7RTRY12JfMWFpAy6FQV8M]
+# equiplanoscf@gmail.com[2Vi8MYIwRvifQcHLejg4zUCepWz_3V1nB8qCcw6pVATDhRQQg]
+# Richielly = [21YTaWNEb99ey9jMhzHpd_5pdd1qgPoJNcTNryx1SpJ]
